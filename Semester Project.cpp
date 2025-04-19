90b88f8976da35a65bc0de1046809335ed07833a
HumairAli26 <humairali2612@gmail.com>
2025-04-19 11:38:52 +0500

    Fixed Errors of Voter Class
    Removed constant function and use of private (inaccessible) variables from voter class. Added voterID and corrected the display function.

--- Semester Project.cpp
+++ Semester Project.cpp
@@ -1,57 +1,121 @@
-#include<iostream>
-
+#include <iostream>
 using namespace std;
-
-class User
+class user 
 {
 private:
     string name;
     int age;
-    string cnic;
     string city;
+    int cnic;
 public:
-    void setname()
+    user()
     {
-        string n;
-        cout<<"Enter Name: ";
-        cin>>n;
+        name="";
+        age=0;
+        city="";
+        cnic=0;
+    }
+    user(string n,int a,string c,int cn)
+    {
         name=n;
-    }
-    void setage()
-    {
-        int a;
-        cout<<"Enter Age: ";
-        cin>>a;
         age=a;
-    }
-    void setcnic()
-    {
-        string nic;
-        cout<<"Enter CNIC: ";
-        cin>>nic;
-        cnic=nic;
-    }
-    void setcity()
-    {
-        string c;
-        cout<<"Enter City: ";
-        cin>>c;
         city=c;
+        cnic=cn;
     }
-    string getname()
+    void setname(string n) 
     {
+        cout << "enter your name :" << endl;
+        cin >> n;
+        name = n;
+    }
+    void setage(int a) {
+        cout << "enter your age :" << endl;
+        cin >> a;
+        age = a;
+    }
+    void setcity(string c) {
+        cout << "enter city name :" << endl;
+        cin >> c;
+        city = c;
+    }
+    void setcnic(int cn) {
+        cout << "enter your cnic :" << endl;
+        cin >> cn;
+        cnic = cn;
+    }
+    string getname() const {
         return name;
     }
-    int getage()
-    {
+    int getage() {
         return age;
     }
-    string getcnic()
-    {
+    string getcity() {
+        return city;
+    }
+    int getcnic() {
         return cnic;
     }
-    string getcity()
+    void displayuserinfo() {
+        
+}
+};
+
+class candidate :public user 
+{
+private:
+    string electsymbol;
+    int specialno;
+public:
+    candidate(string n, int a, string c, int cn, string es, int sn) :user(n, a, c, cn), specialno(sn), electsymbol(es) {};
+    int getspecialno() 
     {
-        return city;
+        return specialno;
+    }
+    string getelectsymbol() 
+    {
+        return electsymbol;
+    }
+};
+
+class voter : public user
+{
+private:
+    string voterID;
+    bool hasvoted;
+public:
+    voter() : user(), voterID(""), hasvoted(false) {}
+
+    voter(string n, int a, string c, int cn, string vid): user(n, a, c, cn), voterID(vid), hasvoted(false) {}
+
+    void sethasvoted(bool voted) 
+    {
+        hasvoted = voted;
+    }
+
+    bool gethasvoted() const 
+    {
+        return hasvoted;
+    }
+
+    void castvote() 
+    {
+        if (!hasvoted) 
+        {
+            hasvoted = true;
+            cout << getname() << " has successfully voted!" << endl;
+        } else 
+        {
+            cout << getname() << " has already voted!" << endl;
+        }
+    }
+
+    void displayinfo()
+    {
+        cout << "Name: " << getname() << endl;
+        cout << "Age: " << getage() << endl;
+        cout << "City: " << getcity() << endl;
+        cout << "CNIC: " << getcnic() << endl;
+        cout << "Voter ID: " << voterID << endl;
+        cout << "Has Voted: " << (hasvoted ? "Yes" : "No") << endl;
     }
 };
